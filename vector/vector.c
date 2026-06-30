7ef2d0a8ce855ce5e3e3c16fef322852a026db8d
AdityaNath1221 <adityanath1221@gmail.com>
2026-06-30 13:35:08 +0530

    Implement generic vector

--- vector/vector.c
+++ vector/vector.c
@@ -1,16 +1,27 @@
 #include "vector.h"
+#include <stddef.h>
+#include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
 
-Vector *vector_create() {
+Vector *vector_create(size_t elem_size) {
+    if (elem_size == 0) {
+        return NULL; // Element size cannot be 0
+    }
     Vector *v = (Vector *)malloc(sizeof(Vector));
     if (v != NULL) {
-        v->capacity = DEFAULT_CAPACITY;
-        v->size = 0;
-        v->data = (int *)malloc(v->capacity * sizeof(int));
+        if (DEFAULT_CAPACITY > SIZE_MAX / elem_size) {
+            free(v);
+            return NULL;
+        }
+        v->data = malloc(DEFAULT_CAPACITY * elem_size);
         if (v->data == NULL) {
             free(v);
             return NULL;
         }
+        v->capacity = DEFAULT_CAPACITY;
+        v->size = 0;
+        v->element_size = elem_size;
     }
     return v;
 }
@@ -22,200 +33,218 @@ void vector_destroy(Vector *vec) {
     }
 }
 
-int vector_size(Vector *vec) {
+size_t vector_size(const Vector *vec) {
     if (vec == NULL) {
-        return VECTOR_ERROR_NULL_POINTER;
-    } else {
-        return vec->size;
-    }
-}
-
-int vector_capacity(Vector *vec) {
-    if (vec == NULL) {
-        return VECTOR_ERROR_NULL_POINTER;
-    } else {
-        return vec->capacity;
-    }
-}
-
-int vector_empty(Vector *vec) {
-    if (vec == NULL) {
-        return VECTOR_ERROR_NULL_POINTER;
-    } else if (vec->size == 0) {
-        return 1;
-    } else {
         return 0;
     }
+    return vec->size;
 }
 
-int vector_full(Vector *vec) {
+size_t vector_capacity(const Vector *vec) {
     if (vec == NULL) {
-        return VECTOR_ERROR_NULL_POINTER;
-    } else if (vec->size == vec->capacity) {
-        return 1;
-    } else {
         return 0;
     }
+    return vec->capacity;
 }
 
-int *vector_front(Vector *vec) {
-    if (vec != NULL && !vector_empty(vec)) {
-        return vec->data;
+size_t vector_element_size(const Vector *vec) {
+    if (vec == NULL) {
+        return 0;
     }
-    return NULL;
+    return vec->element_size;
 }
 
-int *vector_back(Vector *vec) {
-    if (vec != NULL && !vector_empty(vec)) {
-        return vec->data + (vec->size - 1);
+int vector_empty(const Vector *vec) {
+    if (vec == NULL) {
+        return VECTOR_ERROR_NULL_POINTER;
     }
-    return NULL;
-}
-
-int *vector_at(Vector *vec, int pos) {
-    if (vec != NULL && !vector_empty(vec)) {
-        if (pos >= vec->size || pos < 0) {
-            return NULL;
-        } else {
-            return vec->data + pos;
-        }
+    if (vec->size == 0) {
+        return 1;
     }
-    return NULL;
+    return 0;
 }
 
-int vector_push_back(Vector *vec, int data) {
+int vector_full(const Vector *vec) {
     if (vec == NULL) {
         return VECTOR_ERROR_NULL_POINTER;
     }
     if (vec->size == vec->capacity) {
-        int new_capacity = vec->capacity * 2;
-        if (vector_reserve(vec, new_capacity)) {
+        return 1;
+    }
+    return 0;
+}
+
+void *vector_front(const Vector *vec) {
+    if (vec == NULL) {
+        return NULL;
+    }
+    if (vec->size == 0) {
+        return NULL;
+    }
+    return vec->data;
+}
+
+void *vector_back(const Vector *vec) {
+    if (vec == NULL) {
+        return NULL;
+    }
+    if (vec->size == 0) {
+        return NULL;
+    }
+    return (void *)((char *)vec->data + (vec->size - 1) * vec->element_size);
+}
+
+void *vector_at(const Vector *vec, size_t pos) {
+    if (vec == NULL) {
+        return NULL;
+    }
+    if (pos >= vec->size) {
+        return NULL;
+    }
+    return (void *)((char *)vec->data + pos * vec->element_size);
+}
+
+int vector_push_back(Vector *vec, const void *data) {
+    if (vec == NULL) {
+        return VECTOR_ERROR_NULL_POINTER;
+    }
+    if (data == NULL) {
+        return VECTOR_ERROR_INVALID_DATA;
+    }
+    if (vec->size == vec->capacity) {
+        if (vec->capacity > SIZE_MAX / 2) {
+            return VECTOR_ERROR_OVERFLOW;
+        }
+        if (vector_reserve(vec, vec->capacity * 2)) {
             return VECTOR_ERROR_OUT_OF_MEMORY;
         }
     }
-    vec->data[vec->size] = data;
+    memcpy((char *)vec->data + vec->size * vec->element_size, data,
+           vec->element_size);
     vec->size++;
-    return 0;
+    return VECTOR_SUCCESS;
 }
 
 int vector_pop_back(Vector *vec) {
     if (vec == NULL) {
         return VECTOR_ERROR_NULL_POINTER;
-    } else if (vector_empty(vec)) {
+    }
+    if (vec->size == 0) {
         return VECTOR_ERROR_EMPTY;
     }
     vec->size--;
-    return 0;
+    return VECTOR_SUCCESS;
 }
 
-int vector_insert(Vector *vec, int data, int pos) {
-    if (vec != NULL) {
-        if ((vector_empty(vec) && pos == 0) || pos == vec->size) {
-            if (vector_push_back(vec, data) == VECTOR_ERROR_OUT_OF_MEMORY) {
-                return VECTOR_ERROR_OUT_OF_MEMORY;
-            }
-        } else if (pos > vec->size || pos < 0) {
-            return VECTOR_ERROR_INVALID_INDEX;
-        } else {
-            if (vec->size == vec->capacity) {
-                int new_capacity = vec->capacity * 2;
-                if (vector_reserve(vec, new_capacity)) {
-                    return VECTOR_ERROR_OUT_OF_MEMORY;
-                }
-            }
-            int ele = data;
-            for (int i = pos; i < vec->size; i++) {
-                int temp = vec->data[i];
-                vec->data[i] = ele;
-                ele = temp;
-            }
-            vec->data[vec->size] = ele;
-            vec->size++;
-        }
-        return 0;
+int vector_insert(Vector *vec, void *data, size_t pos) {
+    if (vec == NULL) {
+        return VECTOR_ERROR_NULL_POINTER;
     }
-    return VECTOR_ERROR_NULL_POINTER;
+    if (data == NULL) {
+        return VECTOR_ERROR_INVALID_DATA;
+    }
+    if (pos > vec->size) {
+        return VECTOR_ERROR_INVALID_INDEX;
+    }
+    if (pos == vec->size) {
+        return vector_push_back(vec, data);
+    }
+    if (vec->size == vec->capacity) {
+        if (vec->capacity > SIZE_MAX / 2 ||
+            vec->capacity * 2 > SIZE_MAX / vec->element_size) {
+            return VECTOR_ERROR_OUT_OF_MEMORY;
+        }
+        if (vector_reserve(vec, vec->capacity * 2)) {
+            return VECTOR_ERROR_OUT_OF_MEMORY;
+        }
+    }
+    memmove((char *)vec->data + (pos + 1) * vec->element_size,
+            (char *)vec->data + pos * (vec->element_size),
+            (vec->size - pos) * vec->element_size);
+    memcpy((char *)vec->data + pos * (vec->element_size), data,
+           vec->element_size);
+    vec->size++;
+    return VECTOR_SUCCESS;
 }
 
-int vector_erase(Vector *vec, int pos) {
-    if (vec != NULL) {
-        if (vector_empty(vec)) {
-            return VECTOR_ERROR_EMPTY;
-        } else if (pos >= vec->size || pos < 0) {
-            return VECTOR_ERROR_INVALID_INDEX;
-        } else {
-            for (int i = pos; i < vec->size - 1; i++) {
-                vec->data[i] = vec->data[i + 1];
-            }
-            vec->size--;
-            return 0;
-        }
+int vector_erase(Vector *vec, size_t pos) {
+    if (vec == NULL) {
+        return VECTOR_ERROR_NULL_POINTER;
     }
-    return VECTOR_ERROR_NULL_POINTER;
+    if (pos >= vec->size) {
+        return VECTOR_ERROR_INVALID_INDEX;
+    }
+    memmove((char *)vec->data + pos * vec->element_size,
+            (char *)vec->data + (pos + 1) * vec->element_size,
+            (vec->size - pos - 1) * vec->element_size);
+    vec->size--;
+    return VECTOR_SUCCESS;
 }
 
 int vector_clear(Vector *vec) {
     if (vec == NULL) {
         return VECTOR_ERROR_NULL_POINTER;
-    } else {
-        vec->size = 0;
-        return 0;
     }
+    vec->size = 0;
+    return VECTOR_SUCCESS;
 }
 
-int vector_reserve(Vector *vec, int new_capacity) {
+int vector_reserve(Vector *vec, size_t new_capacity) {
     if (vec == NULL) {
         return VECTOR_ERROR_NULL_POINTER;
-    } else {
-        if (new_capacity > vec->capacity) {
-            int *arr = realloc(vec->data, new_capacity * sizeof(int));
-            if (arr == NULL) {
-                return VECTOR_ERROR_OUT_OF_MEMORY;
-            }
-            vec->data = arr;
-            vec->capacity = new_capacity;
-            return 0;
-        }
-        return VECTOR_ERROR_INVALID_CAPACITY;
     }
+    if (new_capacity <= vec->capacity) {
+        return VECTOR_SUCCESS;
+    }
+    if (new_capacity > SIZE_MAX / vec->element_size) {
+        return VECTOR_ERROR_OUT_OF_MEMORY;
+    }
+    void *arr = realloc(vec->data, new_capacity * vec->element_size);
+    if (arr == NULL) {
+        return VECTOR_ERROR_OUT_OF_MEMORY;
+    }
+    vec->data = arr;
+    vec->capacity = new_capacity;
+    return VECTOR_SUCCESS;
 }
 
-int vector_resize(Vector *vec, int new_size) {
+int vector_resize(Vector *vec, size_t new_size) {
     if (vec == NULL) {
         return VECTOR_ERROR_NULL_POINTER;
-    } else {
-        if (new_size > vec->capacity) {
-            if (vector_reserve(vec, new_size)) {
-                return VECTOR_ERROR_OUT_OF_MEMORY;
-            }
-        }
-        vec->size = new_size;
-        return 0;
     }
+    if (new_size > vec->capacity) {
+        if (vector_reserve(vec, new_size)) {
+            return VECTOR_ERROR_OUT_OF_MEMORY;
+        }
+    }
+    vec->size = new_size;
+    return VECTOR_SUCCESS;
 }
 
 int vector_shrink_to_fit(Vector *vec) {
     if (vec == NULL) {
         return VECTOR_ERROR_NULL_POINTER;
-    } else {
-        if (vec->size == vec->capacity) {
-            return 0;
-        } else if (vec->size == 0 && vec->capacity > DEFAULT_CAPACITY) {
-            int *arr = realloc(vec->data, DEFAULT_CAPACITY * sizeof(int));
-            if (arr == NULL) {
-                return VECTOR_ERROR_OUT_OF_MEMORY;
-            }
-            vec->data = arr;
-            vec->capacity = DEFAULT_CAPACITY;
-            return 0;
-        } else if (vec->size < vec->capacity && vec->size > 0) {
-            int *arr = realloc(vec->data, vec->size * sizeof(int));
-            if (arr == NULL) {
-                return VECTOR_ERROR_OUT_OF_MEMORY;
-            }
-            vec->data = arr;
-            vec->capacity = vec->size;
-            return 0;
-        }
     }
+    if (vec->size == vec->capacity) {
+        return VECTOR_SUCCESS;
+    }
+    if (vec->size == 0 && vec->capacity > DEFAULT_CAPACITY) {
+        void *arr = realloc(vec->data, DEFAULT_CAPACITY * vec->element_size);
+        if (arr == NULL) {
+            return VECTOR_ERROR_OUT_OF_MEMORY;
+        }
+        vec->data = arr;
+        vec->capacity = DEFAULT_CAPACITY;
+        return VECTOR_SUCCESS;
+    }
+    if (vec->size < vec->capacity && vec->size > 0) {
+        void *arr = realloc(vec->data, vec->size * vec->element_size);
+        if (arr == NULL) {
+            return VECTOR_ERROR_OUT_OF_MEMORY;
+        }
+        vec->data = arr;
+        vec->capacity = vec->size;
+        return VECTOR_SUCCESS;
+    }
+    return VECTOR_SUCCESS;
 }
--- vector/vector.h
+++ vector/vector.h
@@ -1,3 +1,5 @@
+#include <stddef.h>
+
 #ifndef VECTOR_H
 #define VECTOR_H
 
@@ -8,39 +10,43 @@
 #define VECTOR_ERROR_INVALID_INDEX -97
 #define VECTOR_ERROR_EMPTY -96
 #define VECTOR_ERROR_INVALID_CAPACITY -95
+#define VECTOR_ERROR_INVALID_DATA -94
+#define VECTOR_ERROR_OVERFLOW -93
 
 #define DEFAULT_CAPACITY 4
 
 // DATA STRUCTURE
 typedef struct Vector {
-    int *data;
-    int size;
-    int capacity;
+    void *data;
+    size_t size;
+    size_t capacity;
+    size_t element_size;
 } Vector;
 
 // CONSTRUCTORS/DESTRUCTORS
-Vector *vector_create();
+Vector *vector_create(size_t ele_size);
 void vector_destroy(Vector *vec);
 
 // METAINFO
-int vector_size(Vector *vec);
-int vector_capacity(Vector *vec);
-int vector_empty(Vector *vec);
-int vector_full(Vector *vec);
+size_t vector_size(const Vector *vec);
+size_t vector_capacity(const Vector *vec);
+size_t vector_element_size(const Vector *vec);
+int vector_empty(const Vector *vec);
+int vector_full(const Vector *vec);
 
 // ELEMENT ACCESS
-int *vector_front(Vector *vec);
-int *vector_back(Vector *vec);
-int *vector_at(Vector *vec, int pos);
+void *vector_front(const Vector *vec);
+void *vector_back(const Vector *vec);
+void *vector_at(const Vector *vec, size_t pos);
 
 // MODIFIERS
-int vector_push_back(Vector *vec, int data);
+int vector_push_back(Vector *vec, const void *data);
 int vector_pop_back(Vector *vec);
-int vector_insert(Vector *vec, int data, int pos);
-int vector_erase(Vector *vec, int pos);
+int vector_insert(Vector *vec, void *data, size_t pos);
+int vector_erase(Vector *vec, size_t pos);
 int vector_clear(Vector *vec);
-int vector_reserve(Vector *vec, int new_capacity);
-int vector_resize(Vector *vec, int new_size);
+int vector_reserve(Vector *vec, size_t new_capacity);
+int vector_resize(Vector *vec, size_t new_size);
 int vector_shrink_to_fit(Vector *vec);
 
 #endif
